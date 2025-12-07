54c6543ffc36efddfef3901269d98e346003b8d8
John Nkp <john.nkp1@gmail.com>
2025-12-07 18:48:02 +1100

    minor refactoring to typescript

--- react-markdown.d.ts
+++ react-markdown.d.ts
@@ -1,12 +1,12 @@
-// declare module 'react-markdown' {
-//   import { ReactNode } from 'react';
+declare module 'react-markdown' {
+  import { ReactNode } from 'react';
   
-//   export interface ReactMarkdownProps {
-//     children: string;
-//     className?: string;
-//     [key: string]: any;
-//   }
+  export interface ReactMarkdownProps {
+    children: string | ReactNode;
+    className?: string;
+    [key: string]: any;
+  }
   
-//   const ReactMarkdown: React.FC<ReactMarkdownProps>;
-//   export default ReactMarkdown;
-// }
+  const ReactMarkdown: React.FC<ReactMarkdownProps>;
+  export default ReactMarkdown;
+}
